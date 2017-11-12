84116f55c089d5023e3e5293bfce5717da63e9ff
Kewley, Mark <mark.kewley@windlogics.com>
2017-11-12 15:44:18 -0600

    Added login/createuser

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Card, CardSection, Button, Input } from './components/common';
 
 export default class Loginform extends Component {
-  state = {email: '', password: ''};
+  state = {email: '', password: '', errorMessage: undefined};
+
+  async handleLoginError({code, message}) {
+    if (code === 'auth/user-not-found') {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password);
+        this.setState({errorMessage: undefined});
+      } catch (err) {
+        this.setState({errorMessage: err.message});
+      }
+    } else if (code === 'auth/wrong-password') {
+      this.setState({errorMessage: 'Invalid password'});
+    } else if (code === 'auth/invalid-email') {
+      this.setState({errorMessage: 'Invalid email'});
+    } else if (code === 'auth/user-disabled') {
+      this.setState({errorMessage: 'This user has been disabled'});
+    } else {
+      console.log('UNKNOWN ERROR');
+      this.setState({errorMessage: message});
+    }
+  }
+
+  async handleLogin () {
+    const {email, password} = this.state;
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      this.setState({errorMessage: undefined});
+    } catch (err) {
+      await this.handleLoginError(err);
+    }
+  }
 
   render () {
     return (
@@ -26,8 +58,12 @@ export default class Loginform extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.state.errorMessage}
+        </Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={() => this.handleLogin()}>
             Login
           </Button>
         </CardSection>
@@ -35,3 +71,11 @@ export default class Loginform extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
